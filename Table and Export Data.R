1469decca21543e1459a78fe51b5347d82f1f5b8
VM-GHub <68404902+VM-GHub@users.noreply.github.com>
2020-12-03 17:38:18 -0800

    Update Table and Export Data.R

--- Table and Export Data.R
+++ Table and Export Data.R
@@ -9,8 +9,8 @@ rownames(smoke) <- c("current","former","never")
 smoke
 
 #Export table data as csv file using write.table and is saved to current
-#working directory C:\Users\mail2\Documents
+#working directory C:\Users\UserName\Documents
 write.table(smoke, file="smoke.csv",sep=",")
 
 #Specify the path for where to save the file instead
-write.table(smoke, file="/Users/mail2/Documents/R Dataset for R Tutorials/smoke.csv",sep=",")
+write.table(smoke, file="/Users/UserName/Documents/R Dataset for R Tutorials/smoke.csv",sep=",")
